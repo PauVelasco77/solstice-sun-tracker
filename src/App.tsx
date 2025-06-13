d00fc4f8d1d42568918e516d4355c8817479858a
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-13 10:12:39 +0200

    Add solstice date state and integrate SolsticeCountdown component in App; update useEffect to calculate longest day based on sun timing data.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { useApi } from './hooks/useApi';
 import { SunTimingResults } from './types/sunTiming';
 import { useSolstice } from './hooks/useSolstice';
 import { useGeolocation } from './hooks/useGeolocation';
+import { SolsticeCountdown } from './components/features/solstice-countdown';
 
 function App() {
   const [sunTiming, setSunTiming] = useState<SunTimingResults[] | null>(null);
   const [geolocation, setGeolocation] = useState<GeolocationPosition | null>(
     null,
   );
+  const [solsticeDate, setSolsticeDate] = useState<Date | null>(null);
   const { getSunTimingRange } = useApi();
   const { getLongestDay } = useSolstice();
   const { getGeolocation } = useGeolocation();
@@ -26,11 +28,19 @@ function App() {
         date_end: '2025-06-30',
       }).then(setSunTiming);
     }
-  }, [getSunTimingRange, getLongestDay, geolocation]);
+  }, [geolocation, getSunTimingRange]);
+
+  useEffect(() => {
+    if (sunTiming) {
+      const longestDay = getLongestDay(sunTiming);
+      setSolsticeDate(new Date(longestDay.date));
+    }
+  }, [sunTiming, getLongestDay]);
 
   return (
     <div>
       <h1>Today's Daylight Info</h1>
+      {solsticeDate && <SolsticeCountdown solsticeDate={solsticeDate} />}
       <div>
         {sunTiming?.map((sunTiming) => (
           <div key={sunTiming.date}>
