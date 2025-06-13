054e724dd520c31605ba7ba0ebb9fcd839d04480
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-13 18:25:55 +0200

    Replace SolsticeCountdown component with HeroCountdown, enhancing countdown display with improved layout and formatting options using NumberFlow.

---
+++ src/components/features/hero-section/hero-countdown.tsx/hero-countdown.tsx
@@ -0,0 +1,81 @@
+import NumberFlow, { continuous } from '@number-flow/react';
+import { useEffect, useState } from 'react';
+
+interface HeroCountdownProps {
+  solsticeDate: Date;
+}
+
+export const HeroCountdown = ({ solsticeDate }: HeroCountdownProps) => {
+  const [countdown, setCountdown] = useState<number>(
+    solsticeDate.getTime() - Date.now(),
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCountdown(solsticeDate.getTime() - Date.now());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [solsticeDate]);
+
+  const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countdown % (1000 * 60)) / 1000);
+
+  return (
+    <div className="flex min-w-full items-center gap-12">
+      <div>
+        <NumberFlow
+          className="text-8xl font-medium tracking-tighter whitespace-pre-wrap text-black dark:text-white"
+          value={days}
+          format={{
+            style: 'unit',
+            unitDisplay: 'short',
+            unit: 'day',
+            minimumIntegerDigits: 2,
+          }}
+        />
+      </div>
+      <div>
+        <NumberFlow
+          className="text-8xl font-medium tracking-tighter whitespace-pre-wrap text-black dark:text-white"
+          value={hours}
+          format={{
+            style: 'unit',
+            unitDisplay: 'short',
+            unit: 'hour',
+            minimumIntegerDigits: 2,
+          }}
+        />
+      </div>
+      <div>
+        <NumberFlow
+          plugins={[continuous]}
+          className="text-8xl font-medium tracking-tighter whitespace-pre-wrap text-black dark:text-white"
+          value={minutes}
+          format={{
+            style: 'unit',
+            unitDisplay: 'short',
+            unit: 'minute',
+            minimumIntegerDigits: 2,
+          }}
+        />
+      </div>
+      <div>
+        <NumberFlow
+          plugins={[continuous]}
+          className="text-8xl font-medium tracking-tighter whitespace-pre-wrap text-black dark:text-white"
+          value={seconds}
+          format={{
+            style: 'unit',
+            unitDisplay: 'short',
+            unit: 'second',
+            minimumIntegerDigits: 2,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
