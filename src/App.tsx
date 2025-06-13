3246727c428d3e3d961440b6ad83e1fa6a908308
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-13 19:19:54 +0200

    Refactor App component to utilize Suspense and useMemo for asynchronous data fetching; replace existing state management with promises for sun timing and solstice date, and integrate LandingTemplate for improved UI structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,42 @@
-import { useEffect, useState } from 'react';
+import { Suspense, useMemo } from 'react';
+import LandingTemplate from './components/templates/landing-template';
 import { useApi } from './hooks/useApi';
-import { SunTimingResults } from './types/sunTiming';
 import { useSolstice } from './hooks/useSolstice';
 import { useGeolocation } from './hooks/useGeolocation';
-import { SolsticeCountdown } from './components/features/solstice-countdown';
 
-function App() {
-  const [sunTiming, setSunTiming] = useState<SunTimingResults[] | null>(null);
-  const [geolocation, setGeolocation] = useState<GeolocationPosition | null>(
-    null,
-  );
-  const [solsticeDate, setSolsticeDate] = useState<Date | null>(null);
+export default function App() {
   const { getSunTimingRange } = useApi();
   const { getLongestDay } = useSolstice();
   const { getGeolocation } = useGeolocation();
 
-  useEffect(() => {
-    getGeolocation().then(setGeolocation);
-  }, [getGeolocation]);
+  const sunTimingRangePromise = useMemo(async () => {
+    const geoLocation = await getGeolocation();
+    if (!geoLocation) throw new Error('No geolocation');
 
-  useEffect(() => {
-    if (geolocation) {
-      getSunTimingRange({
-        lat: geolocation?.coords.latitude,
-        lng: geolocation?.coords.longitude,
-        date_start: '2025-06-12',
-        date_end: '2025-06-30',
-      }).then(setSunTiming);
-    }
-  }, [geolocation, getSunTimingRange]);
+    return getSunTimingRange({
+      lat: geoLocation?.coords.latitude,
+      lng: geoLocation?.coords.longitude,
+      date_start: '2025-06-12',
+      date_end: '2025-06-30',
+    });
+  }, [getGeolocation, getSunTimingRange]);
 
-  useEffect(() => {
-    if (sunTiming) {
-      const longestDay = getLongestDay(sunTiming);
-      setSolsticeDate(new Date(longestDay.date));
-    }
-  }, [sunTiming, getLongestDay]);
+  const solsticeDatePromise = useMemo(async () => {
+    const sunTiming = await sunTimingRangePromise;
+    const longestDay = getLongestDay(sunTiming);
+    return new Date(longestDay.date);
+  }, [sunTimingRangePromise, getLongestDay]);
 
   return (
-    <div>
-      <h1>Today's Daylight Info</h1>
-      {solsticeDate && <SolsticeCountdown solsticeDate={solsticeDate} />}
-      <div>
-        {sunTiming?.map((sunTiming) => (
-          <div key={sunTiming.date}>
-            <p>Sunrise: {sunTiming.sunrise}</p>
-            <p>Sunset: {sunTiming.sunset}</p>
-            <p>Day Length: {sunTiming.day_length}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Suspense fallback={<BigSpinner />}>
+      <LandingTemplate
+        solsticeDatePromise={solsticeDatePromise}
+        sunTimingPromise={sunTimingRangePromise}
+      />
+    </Suspense>
   );
 }
 
-export default App;
+function BigSpinner() {
+  return <h2>🌀 Loading...</h2>;
+}
