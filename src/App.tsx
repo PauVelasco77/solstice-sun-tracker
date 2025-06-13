3cf943ae584e866b7aae27866944ea1c57e47229
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-13 09:29:17 +0200

    Integrate geolocation functionality in App component to dynamically fetch sun timing data based on user's location

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,31 @@ import { useEffect, useState } from 'react';
 import { useApi } from './hooks/useApi';
 import { SunTimingResults } from './types/sunTiming';
 import { useSolstice } from './hooks/useSolstice';
+import { useGeolocation } from './hooks/useGeolocation';
 
 function App() {
   const [sunTiming, setSunTiming] = useState<SunTimingResults[] | null>(null);
+  const [geolocation, setGeolocation] = useState<GeolocationPosition | null>(
+    null,
+  );
   const { getSunTimingRange } = useApi();
   const { getLongestDay } = useSolstice();
+  const { getGeolocation } = useGeolocation();
 
   useEffect(() => {
-    getSunTimingRange({
-      lat: 38.907192,
-      lng: -77.036873,
-      date_start: '2025-06-12',
-      date_end: '2025-06-30',
-    }).then((sunTiming) => {
-      setSunTiming(sunTiming);
-      console.log(getLongestDay(sunTiming));
-    });
-  }, [getSunTimingRange, getLongestDay]);
+    getGeolocation().then(setGeolocation);
+  }, [getGeolocation]);
+
+  useEffect(() => {
+    if (geolocation) {
+      getSunTimingRange({
+        lat: geolocation?.coords.latitude,
+        lng: geolocation?.coords.longitude,
+        date_start: '2025-06-12',
+        date_end: '2025-06-30',
+      }).then(setSunTiming);
+    }
+  }, [getSunTimingRange, getLongestDay, geolocation]);
 
   return (
     <div>
