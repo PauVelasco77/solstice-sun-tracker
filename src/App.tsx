eea86f90b1dd75e8a3cd85c8c99b93e3411b127b
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-14 16:21:17 +0200

    Refactor App component to integrate city selection functionality; replace geolocation with localStorage for selected city, and update sun timing calculations accordingly. Add SelectCity component for user interaction when no city is selected.

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,43 @@ import { Suspense, useMemo } from 'react';
 import LandingTemplate from './components/templates/landing-template';
 import { useApi } from './hooks/useApi';
 import { useSolstice } from './hooks/useSolstice';
-import { useGeolocation } from './hooks/useGeolocation';
+import { CITIES } from './data/cities';
+import { SelectCity } from './components/select-city';
+import { useLocalStorage } from './hooks/useLocalStorage';
+
+const LOCAL_STORAGE_KEY = 'solstice-location';
 
 export default function App() {
   const { getSunTimingRange } = useApi();
   const { getLongestDay } = useSolstice();
-  const { getGeolocation } = useGeolocation();
+  const { setValue: setSelectedCity, value: selectedCity } = useLocalStorage<
+    (typeof CITIES)[number] | null
+  >(LOCAL_STORAGE_KEY, null);
 
-  const sunTimingRangePromise = useMemo(async () => {
-    const geoLocation = await getGeolocation();
-    if (!geoLocation) throw new Error('No geolocation');
+  const handleCitySelect = (cityName: string) => {
+    const city = CITIES.find((c) => c.name === cityName);
+    if (city) setSelectedCity(city);
+  };
 
+  const sunTimingRangePromise = useMemo(() => {
+    if (!selectedCity) return Promise.reject(new Error('No location'));
     return getSunTimingRange({
-      lat: geoLocation?.coords.latitude,
-      lng: geoLocation?.coords.longitude,
+      lat: selectedCity.lat,
+      lng: selectedCity.lng,
       date_start: '2025-06-12',
       date_end: '2025-06-30',
     });
-  }, [getGeolocation, getSunTimingRange]);
+  }, [getSunTimingRange, selectedCity]);
 
   const solsticeDatePromise = useMemo(async () => {
     const sunTiming = await sunTimingRangePromise;
     const longestDay = getLongestDay(sunTiming);
     return new Date(longestDay.date);
   }, [sunTimingRangePromise, getLongestDay]);
+
+  if (!selectedCity) {
+    return <SelectCity onValueChange={handleCitySelect} />;
+  }
 
   return (
     <Suspense fallback={<BigSpinner />}>
