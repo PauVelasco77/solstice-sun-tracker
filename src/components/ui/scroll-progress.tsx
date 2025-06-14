9463f2eb7f3b078815045c20a7b7868cf30419f6
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-06-14 16:31:07 +0200

    Add Lens and ScrollProgress components for enhanced UI interactivity; Lens enables customizable zoom functionality, while ScrollProgress provides visual scroll tracking.

--- src/components/ui/scroll-progress.tsx
+++ src/components/ui/scroll-progress.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import { cn } from "@/lib/utils";
-import { motion, MotionProps, useScroll } from "motion/react";
-import React from "react";
+import { cn } from '@/lib/utils';
+import { motion, MotionProps, useScroll } from 'motion/react';
+import React from 'react';
 interface ScrollProgressProps
   extends Omit<React.HTMLAttributes<HTMLElement>, keyof MotionProps> {}
 
@@ -16,7 +16,7 @@ export const ScrollProgress = React.forwardRef<
     <motion.div
       ref={ref}
       className={cn(
-        "fixed inset-x-0 top-0 z-50 h-px origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]",
+        'fixed inset-x-0 top-0 z-50 h-px origin-left bg-gradient-to-r from-[#A97CF8] via-[#F38CB8] to-[#FDCC92]',
         className,
       )}
       style={{
@@ -27,4 +27,4 @@ export const ScrollProgress = React.forwardRef<
   );
 });
 
-ScrollProgress.displayName = "ScrollProgress";
+ScrollProgress.displayName = 'ScrollProgress';
